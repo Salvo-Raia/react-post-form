8e4ae9ebb7fe7975b178639719caabbabdeec58e
Salvo-Raia <salvo.raia@outlook.it>
2026-02-16 18:12:52 +0100

    Init repository + Declaring initialFormData

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+const initialFormdata = {
+  author: "",
+  title: "", 
+  body: "",
+  public: false
+}
+
+  return (
+    <div className='container'>
+      <form>
+
+      </form>
+    </div>
+  )
+}
+
