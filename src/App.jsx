6c3eccb959fa1c603f0ed09619111ad11215f225
Salvo-Raia <salvo.raia@outlook.it>
2026-02-16 19:03:05 +0100

    Add checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,27 @@ export default function App() {
   const [formData, setFormData] = useState(initialFormdata);
   const handleForm = (e) => {
     // Destrutturo target // 
-    const {name, value} = e.target; 
+    const {name, value, type, checked} = e.target; 
 
     setFormData({
       ...formData, 
-      [name]: value, 
+      [name]: type === "checkbox" ? checked : value,
     })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    console.table(formData);
+    
   }
 
   return (
     <div className='container'>
       <h1>Add new post</h1>
-        <form className='bg-light card'>
+        <form className='bg-light card my-3' onSubmit={handleSubmit}>
           <div className='card-body'>
-            <div className='form-group d-flex justify-content-center gap-4'>
-              <div className='author-input'>
+            <div className='form-group d-flex gap-4 '>
+              <div className='author-input text-start'>
               <label className='form-label' htmlFor="author">Author</label>
               <input type="text" 
                      value={formData.author}
@@ -36,7 +42,7 @@ export default function App() {
                      id='author'
                      />
               </div>
-              <div className='title-input'>
+              <div className='title-input text-start'>
               <label className='form-label' htmlFor="title">Title</label>
               <input type="text" 
                      value={formData.title}
@@ -47,9 +53,12 @@ export default function App() {
                      />
               </div>
             </div>
-              <div className='post-input form-group mt-3'>
+              <div className='post-input form-group mt-3 text-start'>
                 <label htmlFor="body" className='form-label'>Post</label>
                 <textarea name="body" value={formData.body} id="body" className='form-control' onChange={handleForm} />
+                <input type="checkbox" id='public' onChange={handleForm} checked={formData.public}/>
+                <label htmlFor="public">Make the post public</label>
+                <button className='btn btn-primary my-3'>Pubblica Post</button>
               </div>
           </div>
         </form>
